8ec4441a7aac049f8decab5fcc60e3c2477adf9d
Alicia Z <aliciaw0@uw.edu>
2024-10-14 14:06:17 -0700

    sprint boot project

---
+++ src/test/java/com/footnote/project/Footnote/FootnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.footnote.project.Footnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
