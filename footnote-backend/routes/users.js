8e6e392f93c9b7b63cd691835232e2113c620a71
Mia Huynh <miahuynh@cs.washington.edu>
2024-10-17 14:20:02 -0700

    login and create

--- footnote-backend/routes/users.js
+++ footnote-backend/routes/users.js
@@ -1,3 +1,8 @@
+/**
+ * Handle all user-related logic
+ */
+
+// defines all the routes related to user operations
 var express = require('express');
 var router = express.Router();
 
@@ -6,4 +11,27 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-module.exports = router;
+// author: Mia
+// Handles the backend of a user login
+// Talks to the database
+const userLogin = (req, res) => {
+  const { username, password } = req.body;
+
+  // logic for user authentication
+  // respond with success or failure
+
+};
+
+// author: Mia
+// Handles the backend of a user create
+// Talks to the database
+const userCreate = (req, res) => {
+  const { username, password } = req.body;
+
+  // logic for creating a new user
+  // respond with success or failure
+
+};
+
+
+module.exports = { userLogin, userCreate, router };
