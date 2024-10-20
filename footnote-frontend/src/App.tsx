83f5c9ed9ae4cace6d579ad19b5ec1c49a168929
Alicia Z <aliciaw0@cs.washington.edu>
2024-10-19 18:21:44 -0700

    super basic video upload functionality implemented

--- footnote-frontend/src/App.tsx
+++ footnote-frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import LoginSignup from "./components/LoginSignup/LoginSignup";
+import VideoUploadPlayer from "./components/VideoPlayer/VideoUploadPlayer";
 
 function App() {
   return (
     <div>
-      <LoginSignup />
+      <VideoUploadPlayer />
     </div>
   );
 }
