5ca653fc11b815feb8b7cb8ea3953b8ea07dfbec
Alicia Z <aliciaw0@cs.washington.edu>
2024-10-21 14:18:25 -0700

    toy code

--- footnote-frontend/src/App.tsx
+++ footnote-frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import LoginSignup from "./components/LoginSignup/LoginSignup";
-import VideoUploadPlayer from "./components/VideoPlayer/VideoUploadPlayer";
+import VideoList from "./components/VideoList/VideoList";
+import VideoPlayback from "./components/VideoPlayback/VideoPlayback";
 
 function App() {
   return (
     <div>
-      <VideoUploadPlayer />
+      <VideoPlayback />
     </div>
   );
 }
