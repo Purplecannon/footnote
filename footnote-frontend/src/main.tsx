9bcd5c114be7018daebce869f5439e6dd46b4e29
Mia Huynh <miahuynh@cs.washington.edu>
2024-11-05 18:16:45 -0800

    resolved merge conflict w documentation branch

--- footnote-frontend/src/main.tsx
+++ footnote-frontend/src/main.tsx
@@ -1,19 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-<<<<<<< HEAD
-import App from "./App.tsx";
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-      {" "}
-      {/* Wrap App with BrowserRouter */}
-      <App />
-=======
 import { Routes } from "./routes/Routes.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Routes />
->>>>>>> Documentation
   </StrictMode>
 );
