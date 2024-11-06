62a1c45688fe3f99f1c9dc1407d40215893f3005
Alicia Z <aliciaw0@uw.edu>
2024-11-05 17:20:40 -0800

    merge documentation

--- footnote-frontend/src/main.tsx
+++ footnote-frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+<<<<<<< HEAD
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
@@ -7,5 +8,12 @@ createRoot(document.getElementById("root")!).render(
       {" "}
       {/* Wrap App with BrowserRouter */}
       <App />
+=======
+import { Routes } from "./routes/Routes.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Routes />
+>>>>>>> Documentation
   </StrictMode>
 );
