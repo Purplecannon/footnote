bca0b0de149716cb05de96b8226c9a9b3f7b02d2
Mia Huynh <miahuynh@cs.washington.edu>
2024-11-05 18:16:45 -0800

    resolved merge conflict w documentation branch

--- footnote-frontend/src/App.tsx
+++ footnote-frontend/src/App.tsx
@@ -2,23 +2,9 @@ import Login from "./pages/Login";
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Login/>} />
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/signup" element={<SignUp/>}/>
-          <Route path="/projects" element={<Homepage />} />
-          <Route path="/projects/new" element={<CreateNewProject />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-=======
     <>
       <Login />
     </>
->>>>>>> Documentation
   );
 }
 
