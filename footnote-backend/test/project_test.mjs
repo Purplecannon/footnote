83009fb0003e045f506c93d47ba048379d9d8a09
Catherine Jin <catherinexjjin@gmail.com>
2024-11-09 13:56:50 -0800

    Additions to annotation and project test

--- footnote-backend/test/project_test.mjs
+++ footnote-backend/test/project_test.mjs
@@ -1,3 +1,47 @@
 //Author: Catherine
 
 import * as assert from 'assert';
+import {getProjects, createProject, addUrl, deleteProject} from '../routes/api/projects.js';
+import {createUser} from "../routes/auth/users.js";
+import {clearTables} from "../config/tables.js";
+import {router} from '../routes/api/videos.js';
+
+//Local: http://localhost:5173/
+
+describe('Successful project creation', () => {
+    beforeEach(async () => {
+        await clearTables();
+    });
+    it('Handles basic creation and video upload', async () => {
+        //create accounts to hold projects
+        //login not implemented as of now
+        const username = 'dummyUser';
+        const password = 'dummyPassword';
+        const userResult = await(createUser(username, password));
+        assert(userResult, 'Created user ' + username.toLowerCase() + '\n');
+
+        //project creation
+        const projectName = 'Project1'
+        const createResult = await(createProject(projectName, username));
+        const expectedResult = "Created project " + projectName + "for user " + username.toLowerCase() + "\n";
+        assert(createResult, expectedResult);
+
+        //video upload
+        
+    });
+    it('Handles basic creation and video upload', async () => {
+        
+    });
+});
+
+describe('Successful get projects', () => {
+    beforeEach(async () => {
+        await clearTables();
+    });
+});
+
+describe('Successful project deletion', () => {
+    beforeEach(async () => {
+        await clearTables();
+    });
+});
