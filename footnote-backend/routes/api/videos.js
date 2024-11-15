188e1603c19d2cfb1112a1534e185005c8de5703
Alicia Z <aliciaw0@cs.washington.edu>
2024-11-14 23:46:04 -0800

    beta release fix

--- footnote-backend/routes/api/videos.js
+++ footnote-backend/routes/api/videos.js
@@ -40,6 +40,7 @@ router.post("/upload-video", upload.single("video"), async (req, res) => {
 
     // Call the S3 upload function to store the file in DigitalOcean Spaces
     const uploadResult = await uploadToS3(file);
+    console.log(uploadResult);
 
     // Update the Url for video with given pid in DigitalOcean Cluser database
     const messageUrl = await addUrl(pid, uploadResult.Location);
