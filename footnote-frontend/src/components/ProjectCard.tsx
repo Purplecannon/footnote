e68e5bf454e56e401671a11c93f5f150430e50bf
Mia Huynh <miahuynh@cs.washington.edu>
2024-11-17 21:27:00 -0800

    beta functionalities + styling updates

--- footnote-frontend/src/components/ProjectCard.tsx
+++ footnote-frontend/src/components/ProjectCard.tsx
@@ -1,34 +1,21 @@
 import React from "react";
-
 import { ProjectData } from "../types/types";
 import "./ProjectCard.css";
-import Card from "react-bootstrap/Card";
 import dancers from "../assets/dancers.png";
-import Button from "react-bootstrap/Button";
 
 interface Props {
   project: ProjectData;
-  buttonText: string;
   onClick: () => void;
 }
 
-const ProjectCard: React.FC<Props> = ({ project, buttonText, onClick }) => {
+const ProjectCard: React.FC<Props> = ({ project, onClick }) => {
   const { title, thumbnailURL } = project;
   const placeholderPic = dancers;
 
   return (
-    <div className="holder">
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={thumbnailURL || placeholderPic} />
-
-        <Card.Body>
-          <Card.Title>{title || "Untitled"}</Card.Title>
-
-          <Button variant="primary" onClick={onClick}>
-            {buttonText}
-          </Button>
-        </Card.Body>
-      </Card>
+    <div className="project-card" onClick={onClick}>
+      <img src={thumbnailURL || placeholderPic} alt={title} />
+      <p className="project-title">{title || "Untitled"}</p>
     </div>
   );
 };
