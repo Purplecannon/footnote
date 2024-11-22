27719f226afee829992aa152b1f33119628a0db7
Alicia Z <aliciaw0@cs.washington.edu>
2024-11-21 17:03:19 -0800

    sync routes

--- footnote-frontend/src/routes/Routes.tsx
+++ footnote-frontend/src/routes/Routes.tsx
@@ -2,16 +2,14 @@ import Login from "../pages/Login/Login";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import UserHome from "../pages/UserHome/UserHome";
 import SignUp from "../pages/SignUp/SignUp";
-import AnnotationPage from "../pages/ProjectPage/ProjectPage";
+import ProjectPage from "../pages/ProjectPage/ProjectPage";
 
 export const router = createBrowserRouter([
   { path: "/", element: <Login /> },
   { path: "/signup", element: <SignUp /> },
   { path: "/home", element: <UserHome /> },
-  { path: "/project", element: <AnnotationPage /> },
-  { path: "/project/:pid", element: <AnnotationPage /> },
-  // { path: "/create-new", element: <CreateNewProject /> },
-  // { path: "/toy", element: <HorizontalExample /> },
+  { path: "/project", element: <ProjectPage /> },
+  { path: "/project/:pid", element: <ProjectPage /> },
 ]);
 
 export function Routes() {
