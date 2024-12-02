310b92f46a42a3d15381c12e2f18200eef5d18c7
Alicia Z <aliciaw0@uw.edu>
2024-12-02 11:10:59 -0800

    don't create tables

--- footnote-backend/app.js
+++ footnote-backend/app.js
@@ -82,12 +82,14 @@ app.use("/annotations", annotationsRouter);
 
 // TODO: change the following initialize() portion once app actually storing real user data
 async function initialize() {
-  try {
-    await createTables();
-    // await clearTables();
-    // await resetTables();
-  } catch (err) {
-    console.log("Error creating/clearing/resetting tables: ", err);
+  if (process.env.NODE_ENV == "development") {
+    try {
+      await createTables();
+      // await clearTables();
+      // await resetTables();
+    } catch (err) {
+      console.log("Error creating/clearing/resetting tables: ", err);
+    }
   }
 }
 
